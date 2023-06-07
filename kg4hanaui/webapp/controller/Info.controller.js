0cf7a262b40337646fa898bc0c658aef00b13fb5
Setu Saxena <setu.saxena@sap.com>
2023-06-07 06:12:11 +0000

    Revert "Revert "SAPUI5 project merging manual""
    This reverts commit 4d75b342dd009b5fe01567488761687a1468a5fa.

---
+++ kg4hanaui/webapp/controller/Info.controller.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+	"com/sap/kg4hana/kg4hanaui/controller/BaseController",
+	"com/sap/kg4hana/kg4hanaui/model/formatter"
+], function (BaseController,formatter) {
+	"use strict";
+	return BaseController.extend("com.sap.kg4hana.kg4hanaui.controller.Info", {
+		formatter: formatter,
+		onInit: function () {
+			this.oDataManager = this.getDataManager();
+			//object match handler
+			this.getRouter().getRoute("info").attachPatternMatched(this._onObjectMatched, this);
+		},
+
+		_onObjectMatched: function (oEvent) {
+			var sURI = oEvent.getParameter("arguments").URI;
+			this.sURI = sURI;
+			this._intializeObjectPage(sURI);
+		},
+
+		_intializeObjectPage: function (sURI) {
+			var oObjectInfoModel = this.oDataManager.onReadObjectInfoByURI(sURI);
+			this.getView().setModel(oObjectInfoModel, "oObjectInfoModel");
+		},
+		
+		onNavGraphPress: function(){
+			this.getRouter().navTo("graph", {
+				URI: this.sURI
+			});
+		},
+
+		onNavigateBack: function () {
+			this.onNavBack();
+		}
+	});
+});
